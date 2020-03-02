dadbef2ae56f1efe4e5d950326d758e7b161161d
William Lim <manvinedi@gmail.com>
2020-03-02 23:56:55 +0800

    Update footer.js

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -3,6 +3,8 @@ import css from "./footer.module.css"
 
 const Footer = () => (
   <footer className={css.footer}>
+    <a href="https://github.com/wlwl2/time-converter">Source</a> <br></br>
+    Built by {` `} <a href="https://www.wlwl2.com/">wlwl2</a> <br></br>
     Built with {` `} <a href="https://www.gatsbyjs.org">Gatsby</a>
   </footer>
 )
