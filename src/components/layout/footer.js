9debf47a5372010bd4bae271cdebcf51804ef6d2
William Lim <manvinedi@gmail.com>
2020-03-08 03:34:06 +0800

    style updates

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -3,9 +3,9 @@ import css from "./footer.module.css"
 
 const Footer = () => (
   <footer className={css.footer}>
-    <a href="https://github.com/wlwl2/time-converter">Source</a> <br></br>
-    Built by {` `} <a href="https://www.wlwl2.com/">wlwl2</a> <br></br>
-    Built with {` `} <a href="https://www.gatsbyjs.org">Gatsby</a>
+    <div className={css.footerLine}><a href="https://github.com/wlwl2/time-converter">Source</a></div>
+    <div className={css.footerLine}>Built by {` `} <a href="https://www.wlwl2.com/">wlwl2</a></div>
+    <div className={css.footerLine}>Built with {` `} <a href="https://www.gatsbyjs.org">Gatsby</a></div>
   </footer>
 )
 
